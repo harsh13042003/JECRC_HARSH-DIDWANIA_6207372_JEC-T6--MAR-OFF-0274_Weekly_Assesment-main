81a7d9b1e13ae4c92e566643907decaf9240ccc5
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-19 22:24:47 +0530

    Update Task1.py

--- Assessment week1_14-03-2026/Task1.py
+++ Assessment week1_14-03-2026/Task1.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-
 driver = webdriver.Chrome()
-
 driver.get("https://the-internet.herokuapp.com/login")
 sleep(2)
-
 print("Website opened")
 
 username = driver.find_element(By.CSS_SELECTOR,"input[name='username']")
@@ -22,5 +19,5 @@ footer = driver.find_element(By.CSS_SELECTOR,"#page-footer a")
 print("Footer link located")
 print(footer.text)
 
-sleep(3)
+sleep(4)
 driver.quit()
